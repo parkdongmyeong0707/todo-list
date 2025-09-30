c5666128314ddf16e351ca815e24a4eef4f54ee2
BaihanW <baihan.wu@mail.utoronto.ca>
2025-09-29 21:35:16 -0400

    Classed that needs implement.

---
+++ src/main/java/csc207/todo_list/JsonStorage.java
@@ -0,0 +1,6 @@
+package csc207.todo_list;
+//This class store data as Json and may contain set-get method only.
+// Need to include all instance needed
+
+public class JsonStorage {
+}
---
+++ src/main/java/csc207/todo_list/TodoListController.java
@@ -0,0 +1,7 @@
+package csc207.todo_list;
+//This class handle events logic of add/del tasks, mark as done,
+// save and load possibly more method, don't forget add instance needed.
+
+public class TodoListController{
+
+}
